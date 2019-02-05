ec409d048b99c9a60c9b9fb9372183b0edf613b8
Timothy Jossund <32945067+timjossund@users.noreply.github.com>
2019-02-05 15:59:39 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$("body").hide();
+$("body").fadeIn(500);
+
+$(".hero-section").hide();
+$(".hero-section").fadeIn(1000);
+$("#herotext1").hide();
+$("#herotext1").fadeIn(1500);
+$("#herotext2").hide();
+$("#herotext2").fadeIn(1500);
